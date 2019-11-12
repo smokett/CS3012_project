28bc9a0e3f5ee1902786d414d8c817f067f89267
heliu <heliu@tcd.ie>
2019-11-12 17:01:45 +0000

    Process comments, gets some output data and uploads them to the database.

--- process_comments.js
+++ process_comments.js
@@ -1,62 +1,113 @@
 //The pouchDB instance, connect to the haskell_stack_pr_comments database from Cloudant.
 var dbRemote = new PouchDB('https://fc535eaf-52c1-47a3-acf6-c990cfa80dfd-bluemix:e01ad0f8a3355ea74bf8efeb523cd6da8e8afe94f5a26b2e6af4a7112dd1d144@fc535eaf-52c1-47a3-acf6-c990cfa80dfd-bluemix.cloudantnosqldb.appdomain.cloud/haskell_stack_pr_comments');
 
+//The db to connect after finish.
+var dbData = new PouchDB('https://fc535eaf-52c1-47a3-acf6-c990cfa80dfd-bluemix:e01ad0f8a3355ea74bf8efeb523cd6da8e8afe94f5a26b2e6af4a7112dd1d144@fc535eaf-52c1-47a3-acf6-c990cfa80dfd-bluemix.cloudantnosqldb.appdomain.cloud/haskell_stack_pr_data');
+
 //The local pouchDb instance
 var dbLocal = new PouchDB('db');
 
 //The amount number of comments.
 var commentsAmount;
 
-//Copy cloudant db to local.
-dbLocal.replicate.from(dbRemote);
-
 //Use a array of Object to contain the result.
 records = [];
 
 
-dbLocal.info(function (err, doc) {
+//Copy cloudant db to local.
+PouchDB.replicate(dbLocal,dbRemote,{
+    retry: true
+}).on('denied',function(err){
+    console.log(err);
+}).on('error',function(err){
+    console.log(err);
+
+}).on('complete',function(info){
+
+
+dbLocal.allDocs({include_docs: true},function (err, raw) {
     if (err) { return console.log(err); }
-    commentsAmount = doc.doc_count;
-}).then(function(){
-for(var i=0;i<commentsAmount;i++)
-{
-    dbLocal.get(i.toString(), function(err, json) {
-        if (err) { return console.log(err); }
+
+    for(var i=0;i<raw.rows.length;i++)
+    {
         var newRecord = true;
         for(var j=0;j<records.length;j++)
         {
-            if(records[j].id === json.id)
+            if(records[j].id === raw.rows[i].doc.id)
             {
-                records[j].comments_received += json.comments.length;
+                records[j].PR_own++;
+                for(var k=0;k<raw.rows[i].doc.comments.length;k++)
+                {
+                    if(raw.rows[i].doc.comments[k].user.id != records[j].id)
+                    {
+                        records[j].comments_received++;
+                    }
+                }
                 newRecord = false;
             }
         }
         if(newRecord)
         {
-            var addRecord = {"login" : json.login,"id" : json.id,"comments_received" : 0,"comments_given" : 0};
+            var addRecord = {"login" : raw.rows[i].doc.login,"id" : raw.rows[i].doc.id,"comments_received" : 0,"comments_given" : 0,"PR_own" : 0,"comments_received_per_PR" : 0};
             records.push(addRecord);
         }
-      });
-}
-}).then(function(){
+    }
 
-for(var i=0;i<commentsAmount;i++)
-{
-    dbLocal.get(i.toString(), function(err, json) {
-        if (err) { return console.log(err); }
-        for(var j=0;j<json.comments.length;j++)
+    for(var i=0;i<raw.rows.length;i++)
+    {
+        for(var j=0;j<raw.rows[i].doc.comments.length;j++)
         {
             for(var k=0;k<records.length;k++)
             {
-                if(json.comments[j].user.id === records[k].id)
+                if(raw.rows[i].doc.comments[j].user.id === records[k].id && raw.rows[i].doc.id != records[k].id)
                 {
                     records[k].comments_given++;
                 }
             }
         }
-      });
-}
 
-}).then(function(){
+    }
+
+    for(var j=0;j<records.length;j++)
+    {
+        if(records[j].PR_own>0)
+        {
+             var PR = records[j].PR_own;
+             var comments = records[j].comments_received;
+             var per = comments/PR;
+            records[j].comments_received_per_PR = records[j].comments_received / records[j].PR_own;
+        }
+    }
+    console.log(records.length);
     console.log(records);
+    dbData.bulkDocs(records).then(function (result) {
+        console.log(result);
+        }).catch(function (err) {
+        console.log(err);
+        });
+     
 });
+
+});
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
